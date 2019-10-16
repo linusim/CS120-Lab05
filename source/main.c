503ef8214282a37707fdfab28ba1ff3395b6cdc5
Linus Im <lim001@ucr.edu>
2019-10-16 10:59:53 -0700

    Worked on Exercise 1

--- source/main.c
+++ source/main.c
@@ -14,10 +14,23 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char led = 0x00;
+	unsigned char button = 0x00;
     /* Insert your solution below */
     while (1) {
-
+	button = ~PINA & 0x0F;
+	if ( (button == 0x01) || (button == 0x02) ) {
+		led = 0x20;
+	} else if ( (button == 0x03) || (button = 0x04) ) {
+		led = 0x30;
+	} else if (button == 0x00) {
+		led = 0x00;
+	}	
+			
+    
+	PORTC = led;	
     }
     return 1;
 }
