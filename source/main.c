ba433d69f49a137a66930801374bb38ac58e2be4
Linus Im <lim001@bolt.cs.ucr.edu>
2019-10-18 17:31:08 -0700

    Worked on exercise 2

--- source/main.c
+++ source/main.c
@@ -12,33 +12,20 @@
 #include "simAVRHeader.h"
 #endif
 
+enum states { init, stepInc, stepDec } State;
+
+void step(
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char led = 0x00;
-	unsigned char button = 0x00;
+	unsigned char buttonA = 0x00;
+	unsigned char buttonB = 0x00;
     /* Insert your solution below */
     while (1) {
-	button = ~PINA & 0x0F;
-	if ( (button == 0x01) || (button == 0x02) ) {
-		led = 0x20;
-	} else if ( (button == 0x03) || (button = 0x04) ) {
-		led = 0x30;
-	} else if ( (button == 0x05) || (button == 0x06) ) {
-		led = 0x38;
-	} else if ( (button == 0x07) || (button == 0x08) || (button == 0x09) ) {
-		led = 0x3C;
-	} else if ( (button == 0x0A) || (button == 0x0B) || (button == 0x0C) ) {
-		led = 0x3E;
-	} else if ( (button == 0x0D) || (button == 0x0E) || (button == 0x0F) ) {
-		led = 0x3F;
-	}  else if (button == 0x00) {
-		led = 0x00;
-	} 	
-			
-    
-	PORTC = led;	
-    }
+	buttonA = ~PINA & 0x01;
+	buttonB = ~PINA & 0x02;	
     return 1;
 }
